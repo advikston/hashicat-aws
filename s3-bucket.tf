9ad13ebb28e82a7538d4ece3c37bafadc3b61ad8
Andrii Kuchmenko <andrii.kuchmenko@gmail.com>
2023-08-23 15:40:13 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket-2023"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/Terraform-hashicorp-labs/s3-bucket/aws"
+  version = "3.15.0"
 }
